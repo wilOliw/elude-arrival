ca6142d16115e8c0c18c6f46c1859b414668c4b9
wiloliw <prgrf@ya.ru>
2020-08-14 16:23:27 +0300

    update mail.php

--- php/mail.php
+++ php/mail.php
@@ -1,27 +1,27 @@
 <?php
-if($_POST) {
+
+if ($_POST) {
   $user = $_POST;
-  $to_Email = 'hello@voynov.co';
+
+  $to_Email = 'hello@elude.co';
   $subject = 'New Elude Request';
-  $headers ="From: Elude Today <hello@eludetoday.com>\nReply-to:hello@eludetoday.com\nContent-Type: text/plain;";
-  $message .= 'Name: '.$user['name'];
-  $message .= "\r\n";
-  $message .= 'E-mail: '.$user['mail'];
-  $message .= "\r\n";
-  $message .= 'City: '.$user['city'];
-  $response;
+  $headers ="From: Elude Today <hello@elude.co>\nReply-to:hello@eludetoday.com\nContent-Type: text/plain;";
 
-  if(!isset($_SERVER['HTTP_X_REQUESTED_WITH']) AND strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) != 'xmlhttprequest') {
-    $response = json_encode(array('text' => 'Error'));
-    die($response);
+  $message = sprintf('Name: %s %sE-mail: %s %sCity: %s',
+    $user['name'],PHP_EOL, $user['mail'], PHP_EOL, $user['city']
+  );
+
+  try {
+      mail($to_Email, $subject, $message, $headers);
+
+      $message = [
+          'text' => sprintf('Hello %s, your message is send.', $user['name'])
+      ];
+  } catch (Exception $exception) {
+      $message = [
+          'text' => $exception->getMessage()
+      ];
   }
 
-  if(!mail($to_Email, $subject, $message, $headers)) {
-    $response = json_encode(array('text' => 'Error.'));
-    die($response);
-    } else {
-    $response = json_encode(array('text' => 'Hello '.$user['name'] .', your message is send.'));
-    die($response);
-  }
+  return json_encode($message);
 }
-?>
